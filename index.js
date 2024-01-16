b7df0a6612c2dccebb00930d645b394164ffd84f
Alayo <sosweetsparke@gmail.com>
2024-01-16 09:46:24 +0100

    new commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 const helmet = require("helmet");
 const cors = require("cors");
 
@@ -16,8 +17,24 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 //  routers
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+
+// middleware
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+
+app.use(express.static("./public"));
+app.use(fileUpload());
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
@@ -31,3 +48,5 @@ const start = async () => {
 };
 
 start();
+
+module.exports = app;
